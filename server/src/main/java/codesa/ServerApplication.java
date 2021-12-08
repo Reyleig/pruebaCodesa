247fe58f8ad8decd1ae04cd976cdc8e51c8f2d31
smorales <86622799+smorales-playtech@users.noreply.github.com>
2021-12-08 10:02:35 -0500

    sin relevancia

--- server/src/main/java/codesa/ServerApplication.java
+++ server/src/main/java/codesa/ServerApplication.java
@@ -1,13 +1,13 @@
-package codesa.prueba;
+package codesa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SerrverApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SerrverApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
--- server/src/main/java/codesa/service/CodesaService.java
+++ server/src/main/java/codesa/service/CodesaService.java
@@ -1,4 +1,4 @@
-package codesa.prueba.service;
+package codesa.service;
 
 public interface CodesaService {
 }
