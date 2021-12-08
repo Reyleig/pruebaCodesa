3b8c5d9b320f86e8e5502427fad4fd321c82d96a
smorales <86622799+smorales-playtech@users.noreply.github.com>
2021-12-08 09:46:32 -0500

    Create SerrverApplicationTests.java

---
+++ server/src/test/java/codesa/prueba/SerrverApplicationTests.java
@@ -0,0 +1,13 @@
+package codesa.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerrverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
