35ee2847da164e228f58e4ba0d542686a657df4e
smorales <86622799+smorales-playtech@users.noreply.github.com>
2021-12-08 09:46:27 -0500

    Create SerrverApplication.java

---
+++ server/src/main/java/codesa/prueba/SerrverApplication.java
@@ -0,0 +1,13 @@
+package codesa.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerrverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerrverApplication.class, args);
+    }
+
+}
